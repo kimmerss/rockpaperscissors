d0b264b687ce2b1059b29d4917b3a52103721107
kimmerss <kimmerssvega@gmail.com>
2019-11-21 22:57:16 +0000

    finish winning section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-//GLOBAL VARIABLES
-/* global $ */
-
+$("#shoot").click(function () {
+  var outcome = Math.random();
+  let userAnswer= $("#input").val();
+  let computerAnswer= "";
+  $("#userChoice").html(answer)
 
 
-// DOCUMENT READY FUNCTION BELOW
+  
+  if (outcome > .60) {
+    $("#computerChoice").html("Rock");
+    computerAnswer="Rock";
+  } else if (outcome <.60 & outcome >.30) {
+    $("#computerChoice").html("Paper");
+    computerAnswer="Paper";
+  } else {
+    $("#computerChoice").html("Scissors");
+    computerAnswer="Scissors";
+  }
+
+if (userAnswer === "rock" && computerAnswer=== "Paper") {
+  $("#result").html("You lost!");
+}
+});
+
 
